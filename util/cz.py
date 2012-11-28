0b3034cdf71e9f3f9e3e67c8cdc3af0dd8f12e08
Petr Prikryl <prikryl@atlas.cz>
2012-11-28 16:14:15 +0100

    Hlavní skript teď přeskakuje první a druhý průchod.

--- util/cz.py
+++ util/cz.py
@@ -9,6 +9,8 @@ import pass2
 import pass3
 import pass4
 
+# Adresář s originálními podadresáři a soubory.
+en_src_dir = os.path.abspath('../../progit/en')
 
 # Pomocné podadresáře pro generované informace.
 cs_aux_dir = os.path.realpath('../info_aux_cs')
@@ -19,36 +21,43 @@ en_aux_dir = os.path.realpath('../info_aux_en')
 if not os.path.isdir(en_aux_dir):
     os.makedirs(en_aux_dir)
 
-# Zpracujeme český překlad z textového souboru, který byl získán
+# Textový soubor, který je prvotním vstupem, který byl získán
 # uložením PDF jako text a následnou ruční úpravou některých jevů,
 # které vznikly ruční sazbou orientovanou na vzhled (tj. nikoliv
-# na zachování struktury dokumentu). Hlavním výsledkem je soubor
-# pass1.txt a vracený slovník toc.
-print('pass 1:')
-parser1 = pass1.Parser('../txtFromPDF/scott_chacon_pro_git_CZ.txt',
-                       cs_aux_dir)
-czTOC, msg = parser1.run()
-print('\t' + msg)
+# na zachování struktury dokumentu).
+csfname_pass1input = '../txtFromPDF/scott_chacon_pro_git_CZ.txt'
 
-# V druhém průchodu rozpoznáváme pass1.txt a generujeme pass2.txt.
-print('pass 2:')
-parser2 = pass2.Parser(os.path.join(cs_aux_dir, 'pass1.txt'), czTOC, cs_aux_dir)
-msg = parser2.run()
-print('\t' + msg)
+# Po manuálních úpravách výše zmíněného souboru dospějeme ke stavu,
+# kdy bude vhodné výsledek uložit a poté manuálně upravovat tento.
+# Používá už markdown syntaxi.
+czfname_pass3input = '../txtCorrected/RucneUpravovanyVysledekPass2.txt'
 
-# Po ručních úpravách zdroje pro první průchod (provedena kontrola
-# pass2.txt lidskýma očima) okopírujeme pass2.txt ručně do odděleného
-# souboru, který budeme dále upravovat ručně. (Kdykoliv je možné srovnat
-# jej s nadále generovaným pass2.txt.) V tomto místě kontrolujeme, zda
-# soubor existuje.
-czfname_pass2man = '../txtCorrected/RucneUpravovanyVysledekPass2.txt'
-if not os.path.isfile(czfname_pass2man):
-    print('\n\n\a\a\aRučně okopírovat pass2.txt do\n\t',
-          repr(czfname_pass2man) + ' !!!\n\n')
-    sys.exit(1)
+# Pokud ručně okopírovaný výsledek druhého průchodu (používaný jako
+# vstup třetího průchodu) dosud neexistuje, zavoláme první dva průchody.
+if not os.path.isfile(czfname_pass3input):
 
-# Adresář s originálními podadresáři a soubory.
-en_src_dir = os.path.abspath('../../progit/en')
+    # Hlavním výsledkem prvního průchodu je soubor pass1.txt a vracený
+    # slovník toc.
+    print('pass 1:')
+    parser1 = pass1.Parser(csfname_pass1input, cs_aux_dir)
+    czTOC, msg = parser1.run()
+    print('\t' + msg)
+
+    # V druhém průchodu rozpoznáváme pass1.txt a generujeme pass2.txt.
+    print('pass 2:')
+    parser2 = pass2.Parser(os.path.join(cs_aux_dir, 'pass1.txt'), czTOC, cs_aux_dir)
+    msg = parser2.run()
+    print('\t' + msg)
+
+    # Po ručních úpravách zdroje pro první průchod (provedena kontrola
+    # pass2.txt lidskýma očima) okopírujeme pass2.txt ručně do odděleného
+    # souboru, který budeme dále upravovat ručně. (Kdykoliv je možné srovnat
+    # jej s nadále generovaným pass2.txt.) V tomto místě kontrolujeme, zda
+    # soubor existuje.
+    if not os.path.isfile(czfname_pass3input):
+        print('\n\n\a\a\aRučně okopírovat pass2.txt do\n\t',
+              repr(czfname_pass3input) + ' !!!\n\n')
+        sys.exit(1)
 
 # V třetím průchodu sesbíráme informace jednak z originálu a jednak
 # z překladu (stejným algoritmem). Vycházíme z čerstvého commitu originálního
@@ -60,7 +69,7 @@ en_src_dir = os.path.abspath('../../progit/en')
 # u některých elementů se porovnává jen druh elementu (existence odstavce,
 # existence odrážky, úroveň nadpisu,...).
 print('pass 3:')
-parser3 = pass3.Parser(czfname_pass2man, en_src_dir, cs_aux_dir, en_aux_dir)
+parser3 = pass3.Parser(czfname_pass3input, en_src_dir, cs_aux_dir, en_aux_dir)
 msg = parser3.run()
 print('\t' + msg)
 
