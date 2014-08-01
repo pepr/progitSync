fa312480cb08f69d3e2fa02d8fb397187ea7d8df
Petr Prikryl <prikryl@atlas.cz>
2014-08-01 23:43:01 +0200

    drobné pokusy o generování jednoho velkého HTML a PDF (bez obrázků)

---
+++ gen_html.bat
@@ -0,0 +1,1 @@
+pandoc -V pagetitle:"Git" -f markdown -t html5 --toc -S -c dip3.css info_aux_cs/pass2.txt -o git.html
---
+++ gen_latex.bat
@@ -0,0 +1,1 @@
+pandoc -V pagetitle:"Git" -f markdown -t latex --toc -S -c dip3.css info_aux_cs/pass2.txt -o git.tex
