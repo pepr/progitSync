eeb4b704e0eb210226e308c8fc28575b32fde3e5
Petr Prikryl <prikryl@atlas.cz>
2014-08-28 13:56:04 +0200

    [cs] revision of another part of ch. 4

--- cs_gen_html.bat
+++ cs_gen_html.bat
@@ -1,1 +1,2 @@
-pandoc -V pagetitle:"ProGit" -f markdown -t html5 --toc -S -c dip3.css cs_aux/single.markdown -o progit.html
+rem pandoc -V pagetitle:"ProGit" -f markdown -t html5 --toc -S -c dip3.css cs_aux/single.markdown -o progit.html
+pandoc -V pagetitle:"ProGit" -f markdown -t html5 -S -c dip3.css cs_aux/single.markdown -o progit.html
